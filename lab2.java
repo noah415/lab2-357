36872e7c275332be8dfe32b2bed83bad2fe69ae4
noah415 <noah.otsuka@gmail.com>
2021-04-14 09:46:16 -0700

    make instruction class

--- lab2.java
+++ lab2.java
@@ -2,6 +2,14 @@ class lab2
 {
   public static void main(String[] args)
   {
-    
+
   }
 }
+
+// superclass: instruction
+// superclass: opcode, print method, constructor
+// addi: opcode, rs, rt, immediate
+// reference dictionary in every each of the 3 unique classes
+// R: dictions: arr[]: [opcode, function]
+// registers can be hard coded into main
+// 32 bit number: instruction = opcode | rs | rt | immediate
