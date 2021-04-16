b3ab4ba5bb752eefdc5c9c9d083a34bcce3d9aab
noah415 <noah.otsuka@gmail.com>
2021-04-16 09:44:59 -0700

    added the hashmap of opcodes

--- lab2.java
+++ lab2.java
@@ -1,8 +1,30 @@
+import java.util.*;
+
 class lab2
 {
+  HashMap<String, int[2]> opcodes = new HashMap<String int[2]>();
+
+  private void initMap()
+  {
+    this.opcodes.put("and", [0,36])
+    this.opcodes.put("or", [0,37])
+    this.opcodes.put("add", [0,32])
+    this.opcodes.put("addi", [8,0])
+    this.opcodes.put("sll", [0,0])
+    this.opcodes.put("sub", [0,34])
+    this.opcodes.put("slt", [0,42])
+    this.opcodes.put("beq", [4,0])
+    this.opcodes.put("bne", [5,0])
+    this.opcodes.put("lw", [35,0])
+    this.opcodes.put("sw", [43,0])
+    this.opcodes.put("j", [2,0])
+    this.opcodes.put("jr", [0,8])
+    this.opcodes.put("jal", [3,0])
+  }
+
   public static void main(String[] args)
   {
-
+    initMap();
   }
 }
 
